ed676a5dbb248db81bb84813cda5e7b2db86fc08
AlexanderWangY <alexander.yisu.wang@outlook.com>
2026-04-22 10:36:45 -0400

    feat: fexit tcp recv implementation

--- bpf/kern.bpf.c
+++ bpf/kern.bpf.c
@@ -5,8 +5,8 @@
 struct proc_meta {
   __u64 bytes_sent;
   __u64 bytes_recv;
-  __u64 pkts_sent;
-  __u64 pkts_recv;
+  __u64 send_calls;
+  __u64 recv_calls;
   __u64 last_seen_ns;
   char comm[16];
 };
@@ -27,14 +27,37 @@ int fexit__tcp_sendmsg(struct sock *sk, struct msghdr *msg, size_t size, int ret
   }
 
   __u64 pid = bpf_get_current_pid_tgid();
-  __u32 tgid = pid << 32;
+  __u32 tgid = pid >> 32;
 
   struct proc_meta *pmeta = bpf_map_lookup_elem(&proc_stats, &tgid);
   if (pmeta) {
         __sync_fetch_and_add(&pmeta->bytes_sent, retval);
+        __sync_fetch_and_add(&pmeta->send_calls, 1);
   } else {
     struct proc_meta new = {};
     new.bytes_sent = retval;
+    new.send_calls = 1;
+    bpf_get_current_comm(&new.comm, sizeof(new.comm));
+    bpf_map_update_elem(&proc_stats, &tgid, &new, BPF_NOEXIST);
+  }
+  return 0;
+}
+
+SEC("fexit/tcp_cleanup_rbf")
+int fexit__tcp_cleanup_rbf(struct sock *sk, int copied) {
+  if (copied <= 0) return 0;
+
+  __u64 pid = bpf_get_current_pid_tgid();
+  __u32 tgid = pid >> 32;
+
+  struct proc_meta *pmeta = bpf_map_lookup_elem(&proc_stats, &tgid);
+  if (pmeta) {
+    __sync_fetch_and_add(&pmeta->bytes_recv, copied);
+    __sync_fetch_and_add(&pmeta->recv_calls, 1);
+  } else {
+    struct proc_meta new = {};
+    new.bytes_recv = copied;
+    new.recv_calls = 1;
     bpf_get_current_comm(&new.comm, sizeof(new.comm));
     bpf_map_update_elem(&proc_stats, &tgid, &new, BPF_NOEXIST);
   }
