5e5e2466a5350ae70954ab4f38a5ffbf58085804
AlexanderWangY <alexander.yisu.wang@outlook.com>
2026-04-22 10:58:23 -0400

    feat: uspace attach and cleanup lifecycle

--- src/main.c
+++ src/main.c
@@ -5,65 +5,49 @@
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-static volatile int running = 1;
+// Used to supress GCC/CLANG warnings
+// https://stackoverflow.com/questions/3599160/how-can-i-suppress-unused-parameter-warnings-in-c/3599170#3599170
+#define UNUSED(x) (void)(x)
 
-void signal_handler(int _) {
-  running = 0;
+static volatile bool running = true;
+
+void signal_handler(int sig) {
+  UNUSED(sig);
+  running = false;
 }
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <interface>\n", argv[0]);
-    return 1;
-  }
-
-  const char *ifname = argv[1];
-  int ifindex = if_nametoindex(ifname);
-  if (!ifindex) {
-    fprintf(stderr, "Failed to get interface index for %s.\n", ifname);
-    return 1;
-  }
+  // These will be used later for CLI flags
+  UNUSED(argc);
+  UNUSED(argv);
+  
+  struct kern_bpf *skel;
+  int err;
 
   signal(SIGINT, signal_handler);
-
-  struct kern_bpf *skel;
+  signal(SIGTERM, signal_handler);
 
   skel = kern_bpf__open_and_load();
   if (!skel) {
     fprintf(stderr, "Failed to load skeleton.\n");
-    return 1;
+    err = 1;
+    goto cleanup;
   }
 
-  if (kern_bpf__attach(skel)) {
+  err = kern_bpf__attach(skel);
+  if (err) {
     fprintf(stderr, "Failed to attach skeleton.\n");
     goto cleanup;
   }
 
-  // if (bpf_xdp_attach(ifindex, bpf_program__fd(skel->progs.count_packets), 0, NULL) < 0) {
-  //   fprintf(stderr, "Failed to attach XDP to %s.\n", ifname);
-  //   goto cleanup;
-  // }
+  while (running) {
+    //TODO: Run through map with get next key, collect in array, then sort and print
+  }
 
-  // __u32 key = 0;
-  // __u64 value = 0;
-
-  // initscr();
-
-  // while (running) {
-  //   int err = bpf_map__lookup_elem(skel->maps.packet_count, &key, sizeof(key),
-  //                                  &value, sizeof(value), 0);
-  //   if (err != 0) continue;
-  //   clear();
-  //   printw("Packets processed: %llu", value);
-  //   refresh();
-  //   sleep(1);
-  // }
-
-  // endwin();
 
 cleanup:
-  // bpf_xdp_detach(ifindex, 0, NULL);
   kern_bpf__destroy(skel);
   return 0;
 }
